182719b92622335165f3309f10187f08908c48aa
hasegawa <iphn.x.96@gmail.com>
2020-12-20 18:32:02 +0900

    add login signin function

--- src/App.js
+++ src/App.js
@@ -10,24 +10,24 @@ const client = new ApolloClient({
   uri: "http://127.0.0.1:8000/graphql/",
   headers: {
     authorization: localStorage.getItem("token")
-      ?`JWT ${localStorage.getItem("token")}`
-      :"",
-  },cache: new InMemoryCache(),
+      ? `JWT ${localStorage.getItem("token")}`
+      : "",
+  },
+  cache: new InMemoryCache(),
 });
 
-
-const App = ()=> {
+const App = () => {
   return (
-    <ApolloProvider client={client}>    
-        <div className={styles.app}>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={Auth} />
-              <Route exact path="/employees" component={MainPage} />
-            </Switch>
-          </BrowserRouter>
-        </div>
+    <ApolloProvider client={client}>
+      <div className={styles.app}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Auth} />
+            <Route exact path="/employees" component={MainPage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     </ApolloProvider>
   );
-}
+};
 export default App;
